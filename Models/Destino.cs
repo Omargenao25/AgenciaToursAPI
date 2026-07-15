336a9f43a59ae7e334d58d55c3e63121f146d13e
Omargenao25 <omargenao78@gmail.com>
2026-07-14 21:10:18 -0400

    creacios de modelo pais y destino y requerimientos

---
+++ Models/Destino.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgenciaToursAPI.Models
+{
+    public class Destino
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del destino es obligatorio.")]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Range(0, 365, ErrorMessage = "La duración en días no puede ser negativa.")]
+        public int DuracionDias { get; set; }
+
+        [Range(0, 23, ErrorMessage = "Las horas deben estar entre 0 y 23.")]
+        public int DuracionHoras { get; set; }
+
+        [Required(ErrorMessage = "Debe seleccionar un país.")]
+        public int PaisId { get; set; }
+
+        public Pais? Pais { get; set; }
+
+        public List<Tour> Tours { get; set; } = new();
+    }
+}
---
+++ Models/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgenciaToursAPI.Models
+{
+    public class Pais
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del país es obligatorio")]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+        public List<Destino> Destinos { get; set; } = new();
+    }
+}
