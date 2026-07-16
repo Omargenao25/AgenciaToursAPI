141c6c104f4308161f1488ad3030a928eb4bdd7d
Omargenao25 <omargenao78@gmail.com>
2026-07-15 23:08:20 -0400

    creacion del DBcontext para conexion con base de datos codeFirst en conjunto de connection string en appsettingsjson y inyeccion de dependencia en el program.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AgenciaToursAPI.Models;
+
+namespace AgenciaToursAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pais> Paises { get; set; }
+
+        public DbSet<Destino> Destinos { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Guia> Guias { get; set; }
+
+        public DbSet<Transporte> Transportes { get; set; }
+
+        public DbSet<Tour> Tours { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<MetodoPago> MetodosPago { get; set; }
+
+        public DbSet<Reserva> Reservas { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
