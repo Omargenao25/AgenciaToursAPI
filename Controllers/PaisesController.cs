83e20c7aee57babb720c91405f94718a69c52344
Omargenao25 <omargenao78@gmail.com>
2026-07-17 16:05:17 -0400

    controlador de paises en proceso

---
+++ Controllers/PaisesController.cs
@@ -0,0 +1,19 @@
+﻿using AgenciaToursAPI.Data;
+using AgenciaToursAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgenciaToursAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaisesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
