3a9eda566b163c260deb29ae67d8691d0ee9d444
Omargenao25 <omargenao78@gmail.com>
2026-07-18 20:41:59 -0400

    avances en el paisController

--- Controllers/PaisesController.cs
+++ Controllers/PaisesController.cs
@@ -15,5 +15,95 @@ namespace AgenciaToursAPI.Controllers
         {
             _context = context;
         }
+
+
+      
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pais>>> GetPaises()
+        {
+            return await _context.Paises.ToListAsync();
+        }
+
+
+    
+        [HttpPost]
+        public async Task<ActionResult<Pais>> PostPais(Pais pais)
+        {
+            _context.Paises.Add(pais);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetPaises),
+                new { id = pais.Id },
+                pais
+            );
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pais>> GetPais(int id)
+        {
+            var pais = await _context.Paises.FindAsync(id);
+
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            return pais;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPais(int id, Pais pais)
+        {
+            if (id != pais.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pais).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaisExiste(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        private bool PaisExiste(int id)
+        {
+            return _context.Paises.Any(p => p.Id == id);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePais(int id)
+        {
+            var pais = await _context.Paises.FindAsync(id);
+
+            if (pais == null)
+            {
+                return NotFound();
+            }
+
+            _context.Paises.Remove(pais);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
