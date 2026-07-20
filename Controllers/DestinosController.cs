17a66c63b99d82c469604f638676c504c158e327
Omargenao25 <omargenao78@gmail.com>
2026-07-20 15:31:10 -0400

    creacion de destinos controller en proceso

---
+++ Controllers/DestinosController.cs
@@ -0,0 +1,29 @@
+﻿using AgenciaToursAPI.Data;
+using AgenciaToursAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgenciaToursAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DestinosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+      
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Destino>>> GetDestinos()
+        {
+            return await _context.Destinos
+                .Include(d => d.Pais)
+                .ToListAsync();
+        }
+    }
+}
